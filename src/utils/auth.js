94438545dfa819acb80261806aaacada134152fc
ayushrudani <ayushrudani9499@gmail.com>
2024-04-03 09:11:14 +0530

    update secret

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,6 +8,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      redirectUri: process.env.GOOGLE_REDIRECT_URI,
     }),
   ],
   secret: process.env.SECRET,
